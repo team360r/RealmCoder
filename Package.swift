8d4f21851acb694f4a0b6961ff69a88a1333b832
team360r <61115256+team360r@users.noreply.github.com>
2020-02-27 17:02:15 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,25 +22,25 @@ let package = Package(
             name: "RealmCoder",
             targets: ["RealmCoder"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/realm/SwiftLint.git",
-            from: "0.39.1"
-        ),
-        .package(
-            url: "https://github.com/realm/realm-cocoa.git",
-            from: "4.3.2"
-        )
-    ],
+//     dependencies: [
+//         // Dependencies declare other packages that this package depends on.
+//         // .package(url: /* package url */, from: "1.0.0"),
+//         .package(
+//             url: "https://github.com/realm/SwiftLint.git",
+//             from: "0.39.1"
+//         ),
+//         .package(
+//             url: "https://github.com/realm/realm-cocoa.git",
+//             from: "4.3.2"
+//         )
+//     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which
         // this package depends on.
         .target(
             name: "RealmCoder",
-            dependencies: ["RealmCoder"]),
+            dependencies: ["RealmSwift"]),
         .testTarget(
             name: "RealmCoderTests",
             dependencies: ["RealmCoder"])
